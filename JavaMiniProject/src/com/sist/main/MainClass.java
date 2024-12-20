2306ebeaaf06328bb96e0b90d43bdf794a459318
sist-108 <sist-108@DESKTOP-KB9HU04>
2024-12-20 14:43:54 +0900

    2222

--- JavaMiniProject/src/com/sist/main/MainClass.java
+++ JavaMiniProject/src/com/sist/main/MainClass.java
@@ -3,6 +3,7 @@ package com.sist.main;
 public class MainClass {
 
 	public static void main(String[] args) {
+		System.out.println();
 		// TODO Auto-generated method stub
 System.out.println();
 	}
